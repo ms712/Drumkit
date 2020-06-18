9b0e722948f342941fcf23d3f92e80b1a623409f
ms712 <39034082+ms712@users.noreply.github.com>
2020-06-18 17:05:00 +0530

    Add files via upload

---
+++ Drum Kit Starting Files/index.js
@@ -0,0 +1,60 @@
+//when button of browser is pressed
+var numberofDrums = document.querySelectorAll(".drum").length;
+for(var i = 0; i < numberofDrums; i++){
+ document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+
+     var buttonWord = this.innerHTML;
+     makeSound(buttonWord);
+     animationButton(buttonWord);
+
+ });
+}
+// if button of keyboard pressed
+document.addEventListener("keydown",function(event){
+      makeSound(event.key);
+         animationButton(event.key);
+})
+function makeSound(key){
+
+      switch (key) {
+           case "w":
+                      var audio1 = new Audio("sounds/tom-1.mp3");
+                      audio1.play();
+                      break;
+           case "a":
+                      var audio2 = new Audio("sounds/tom-2.mp3");
+                      audio2.play();
+                      break;
+           case "s":
+                      var audio3 = new Audio("sounds/tom-3.mp3");
+                      audio3.play();
+                      break;
+           case "d":
+                       var audio4 = new Audio("sounds/tom-4.mp3");
+                       audio4.play();
+                       break;
+          case "j":
+                       var audio5 = new Audio("sounds/snare.mp3");
+                       audio5.play();
+                       break;
+          case "k":
+                      var audio6 = new Audio("sounds/crash.mp3");
+                      audio6.play();
+                      break;
+          case "l":
+                      var audio7 = new Audio("sounds/kick-bass.mp3");
+                      audio7.play();
+                      break;
+
+           default:
+                      console.log(buttonWord);
+
+         }
+}
+function animationButton(currentKey){
+        var addingClass =  document.querySelector("."+currentKey).classList;
+        addingClass.add("pressed");
+        setTimeout(function(){
+           addingClass.remove("pressed");
+        },100);
+}
